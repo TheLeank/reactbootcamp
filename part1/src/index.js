e5b36e0a983ed69f9bf8be463647b5d965e914a9
Jacobo Rodriguez <jvdaw@gmail.com>
2021-06-10 08:31:08 +0000

    part1 exercise 1.2. The 1.1 commited was the 1.2.

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
         {props.part} {props.exercises}
@@ -33,9 +33,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
       <Total a={exercises1} b={exercises2} c={exercises3} />
     </div>
   )
