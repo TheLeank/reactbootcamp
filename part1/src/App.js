9d84971b6a7d02e45a0b190c60e75b5bfce9720b
Jacobo Rodriguez <jvdaw@gmail.com>
2021-06-15 09:27:26 +0000

    Funciones genéricas (func. que devuelven func.)

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,20 +3,20 @@ import React, { useState } from 'react'
 const App = () => {
     const [value, setValue] = useState(10)
 
-
-    const hello = () => {
-        const handler = () => console.log('hello world')
+    // Ahora pasámos un parámetro a hello
+    const hello = (who) => {
+        const handler = () => console.log('hello', who)
         return handler
     }
 
     return (
         <div>
             {value}
-            {/* Normalmente no llamamos a una función desde un event, sin
-            embargo en esta ocasión, llamar a hello() devuelve
-            () => console.log('hello world'), lo que no produce un loop infinito
-            Usar funciones que devuelven funciones es útil en ocasiones */}
-            <button onClick={hello()}>reset to zero</button>
+            {/* Y llamamos a hello dando diferentes parámetros, creando así
+            una función genérica */}
+            <button onClick={hello('world')}>button</button>
+            <button onClick={hello('react')}>button</button>
+            <button onClick={hello('function')}>button</button>
         </div>
     )
 }
