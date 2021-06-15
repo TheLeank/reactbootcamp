dc5e6470482c26266b438d9cc41b6f4d5b398dda
Jacobo Rodriguez <jvdaw@gmail.com>
2021-06-15 08:00:42 +0000

    Refactorizada App.js para usar más componentes

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,36 +2,35 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    // Usando desestructuración, asignamos a counter y setCounter el valor
-    // del estado (0 en este caso) y una función que nos permite alterar ese
-    // estado (e.g. setCounter(5 | counter + 1) respectivamente
     const [counter, setCounter] = useState(0)
 
     const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
     const setToZero = () => setCounter(0)
 
     return (
         <div>
-            {/* En vez de tener el counter en App, refactorizamos para dividir
-            la aplicación en varios componentes. De esta forma, creamos el
-            componente Display, el cuál usaremos para mostrar el valor del 
-            contador */}
+            {/* Dado que ahora usamos componentes, el código es más corto
+            y legible */}
             <Display counter={counter} />
-            {/* Añadimos un handler a un button. Al usar useState y modificar el
-            estado, el componente se re-renderizará. En este caso hemos usado
-            arrow function directamente en el button, pero no es una buena
-            práctica. Lo normal es crear la función en el cuerpo del componente
-            y llamarla en el onClick i.e. onClick={increaseByOne} */}
-            <button onClick={() => setCounter(counter + 1)}>
-                plus
-            </button>
-            {/* En este caso usamos function reference en vez de function call, 
-            que sería setToZero() */}
-            <button onClick={setToZero}>
-                zero
-            </button>
+            <Button handleClick={increaseByOne} text="plus" />
+            <Button handleClick={decreaseByOne} text="minus" />
+            <Button handleClick={setToZero} text="zero" />
         </div>
     )
+}
+
+// Creamos dos componentes, Display y Button, para poder reusarlos
+// Además, podemos usar desestructuración. Sabemos que usaremos la prop counter
+// por tanto podemos reemplazar (props) por ({ counter }) y así, usar dichos 
+// nombres dentro del componente. Recuerda que el orden de los props se define
+// por el orden en que son escritos los atributos al llamar al componente
+const Display = ({ counter }) => {
+    return <div>{counter}</div>
+}
+
+const Button = ({ handleClick, text }) => {
+    return <button onClick={handleClick}>{text}</button>
 }
 
 export default App
