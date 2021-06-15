f59f6a0815a3ec0fbf234839c16499f698a7585a
Jacobo Rodriguez <jvdaw@gmail.com>
2021-06-15 09:21:24 +0000

    Funciones que devuelven funciones en event handler

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,50 +1,24 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    // Esta vez el estado es un array, en el cuál almacenaremos cada click que
-    // realicemos en los botones left y right
-    const [allClicks, setAll] = useState([])
+    const [value, setValue] = useState(10)
 
-    const handleLeftClick = () => {
-        // Además de aumentar el contador left, concatenamos la letra 
-        // correspondiente. Importante: en react no se mutan los datos, sino que
-        // se crean nuevos objetos añadiendo la nueva información. concat
-        // devuelve un nuevo array al que se añade L, pero no modifica
-        // directamente el valor de allClicks. De esto se encarga su función
-        // setAll
 
-        // En javascript también es posible usar allClicks.push('L') y
-        // setAll(allClicks), y de hecho funciona en React, pero .push muta el
-        // array, lo cuál está prohibido en React ya que puede ocasionar 
-        // errores muy difíciles de depurar 
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
+    const hello = () => {
+        const handler = () => console.log('hello world')
+        return handler
     }
 
     return (
         <div>
-            {left}
-            <button onClick={handleLeftClick}>left</button>
-            <button onClick={handleRightClick}>right</button>
-            {right}
-            <p>{allClicks.join(' ')}</p>
+            {value}
+            {/* Normalmente no llamamos a una función desde un event, sin
+            embargo en esta ocasión, llamar a hello() devuelve
+            () => console.log('hello world'), lo que no produce un loop infinito
+            Usar funciones que devuelven funciones es útil en ocasiones */}
+            <button onClick={hello()}>reset to zero</button>
         </div>
     )
-}
-
-const Display = ({ counter }) => {
-    return <div>{counter}</div>
-}
-
-const Button = ({ handleClick, text }) => {
-    return <button onClick={handleClick}>{text}</button>
 }
 
 export default App
