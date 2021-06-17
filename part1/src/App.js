de476a92527dac97e05ea556e7703c81966a317e
Jacobo Rodriguez <jvdaw@gmail.com>
2021-06-17 09:25:57 +0000

    Ejercicios Unicafe Manejo de arrays en estado Sacar mayor valor de un array y su key a partir de ello

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,74 +1,67 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    const [feedback, setFeedback] = useState({
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        all: 0
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+    ]
+    const [selected, setSelected] = useState({
+        value: 0,
+        // Creo un array de la longitud de anecdotes y lo relleno con ceros
+        votes: new Array(anecdotes.length).fill(0),
+        top: 0
     })
 
-    const handleGood = () => {
-        setFeedback({
-            ...feedback,
-            good: feedback.good + 1,
-            all: feedback.all + 1
+    // Handle functions 
+    const handleClick = () => {
+        const r = Math.floor(Math.random() * anecdotes.length)
+        setSelected({
+            ...selected,
+            value: r
         })
     }
-    
-    const handleNeutral = () => {
-        setFeedback({
-            ...feedback,
-            neutral: feedback.neutral + 1,
-            all: feedback.all + 1
+
+    const handleVote = () => {
+        // Copio el array del estado, al cual añado el voto y posteriormente
+        // lo guardo en el estad
+        const newVotes = [...selected.votes]
+        newVotes[selected.value] += 1
+        // Usando spread operator podemos sacar fácilmente el mayor valor del
+        // array
+        const topValue = Math.max(...newVotes)
+        // Dicho valor lo uso en una función para poder compararlo a los valores
+        // del array usando findIndex, que usa como primer parámetro una función
+        // y así, sacar la key del array donde se encuentra ese value
+        const largest = (element) => element === topValue
+        const key = newVotes.findIndex(largest)
+        // Y finalmente actualizo el estado
+        setSelected({
+            ...selected,
+            votes: newVotes,
+            top: key
         })
+        
     }
-    
-    const handleBad = () => {
-        setFeedback({
-            ...feedback,
-            bad: feedback.bad + 1,
-            all: feedback.all + 1
-        })
-    }   
 
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button handleClick={handleGood} text='good' />
-            <Button handleClick={handleNeutral} text='neutral' />
-            <Button handleClick={handleBad} text='bad' />
-            <h1>statistics</h1>
-            <table>
-                <tbody>
-                    <Statistics text='good' value={feedback.good} />
-                    <Statistics text='neutral' value={feedback.neutral} />
-                    <Statistics text='bad' value={feedback.bad} />
-                    <Statistics text='all' value={feedback.all} />
-                    <Statistics text='average' value={(feedback.good - feedback.neutral) / feedback.all} />
-                    <Statistics text='positive' value={(feedback.good * 100) / feedback.all} />
-                </tbody>
-            </table>
+            <h1>anecdote of the day</h1>
+            <p>{anecdotes[selected.value]}</p>
+            <p>has {selected.votes[selected.value]} votes</p>
+            <div>
+                <button onClick={handleVote}>vote</button>
+                <button onClick={handleClick}>next anecdote</button>
+            </div>
+            <h1>anecdote with most votes</h1>
+            <p>{anecdotes[selected.top]}</p>
+            <p>has {selected.votes[selected.top]} votes</p>
         </div>
     )
 }
-
-const Button = ({ handleClick, text }) =>
-        <button onClick={handleClick}>{text}</button>
-    
-
-const Statistics = ({ text, value }) => {
-    if (value === 0) {
-        return <tr><td>No feedback given</td></tr>
-    }
-    
-    return (
-    <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-    </tr>
-    )
-}
-
 
 export default App
