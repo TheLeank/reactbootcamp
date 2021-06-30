f859c1f2de38153202da82bf589a933a368c64b8
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-06-30 11:33:07 +0000

    Ejercicio 2.6 Creamos un listin telefonico en el cual guardamos y mostramos nombres

---
+++ phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    const tempPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(tempPerson))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name} >{person.name}</p>)}       
+    </div>
+  )
+}
+
+export default App
