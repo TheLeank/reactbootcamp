dedb5c723d440ec1039f99f31ffee06ae2d76761
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-07-01 21:01:33 +0000

    Ejercicio 2.9 Filtrado de elementos mostrados

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  // Creo un nuevo estado para almacenar las personas a mostrar
+  const [ personsShown, setPersonsShown ] = useState(persons)
 
   const handleAddPerson = (event) => {
     event.preventDefault()
-    // Comprobamos si en el estado ya existe un objeto cuyo name es el que 
-    // queremos guardar
     if(!persons.find(p => p.name === newName)) {
       const tempPerson = {
         name: newName,
@@ -23,8 +23,6 @@ const App = () => {
       setPersons(persons.concat(tempPerson))
       setNewName('')
     } else {
-      // Usando template string para mostrar el valor de la variable. Notense
-      // las backquotes
       alert(`${newName} is already added to phonebook`)
     }
   }
@@ -37,10 +35,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
   
+  // Seria posible hacer esto con una variable y no con una funcion?
+  const handleFilter = (event) => {
+     event.target.value.length > 0
+      ? setPersonsShown(persons.filter((person) => person.name.search(new RegExp(event.target.value, "i")) !== -1))
+      : setPersonsShown(persons)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      {/* Creo el input y anado un handler */}
+      filter shown with <input type="text" onChange={handleFilter}/>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNewName} /><br/>
@@ -51,7 +58,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name} >{person.name} {person.number}</p>)}       
+      {/* Cambio para mostrar el estado personsShown */}
+      {personsShown.map(person => <p key={person.name} >{person.name} {person.number}</p>)}       
     </div>
   )
 }
