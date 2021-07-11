abddc863b453ad59460a3527b903357cc88efdf3
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-07-11 12:10:28 +0000

    REST POST con axios

--- part2/src/App.js
+++ part2/src/App.js
@@ -3,45 +3,40 @@ import axios from 'axios'
 import Note from './components/Note'
   
 const App = () => {
-  // Inicializar las notas como un array vac'io. Ya no hay props en App que 
-  // hayan sido definidas en index.js
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
 
-  // Se ejecuta primero el componente y, despues, se ejecuta useEffect, que
-  // consta de dos parametros, siendo el primero la funcion o efecto en si
-  // y el segundo, en este caso [], indica la frecuencia con la que se ejecuta
-  // el efecto. Con [] indicamos que se lance unicamente al renderizar el 
-  // componente por primera vez.
+
   useEffect(() => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fullfiled');
         setNotes(response.data)
       })
   }, [])
-  // Esta linea se ejecuta primero con 0 notes y, tras renderizarse el comp App
-  // se vuelve a ejecutar, pues App se re-renderiza por el useEffect (creo)
-  console.log('render', notes.length, 'notes');
-  
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1
+      // Eliminamos el ID, se encarga el server
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      // Hacemos POST del objeto nota, guardando en el server
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        // Asignamos a notas las notas + la nueva nota, que es la respuesta
+        // a la petición realizada
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value)
   }
   
