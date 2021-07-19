716329f84ce02f1217c7e8d381685b6ab44e3c62
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-07-19 11:26:05 +0000

    Preparar para Heroku

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ const generateId = () => {
     return maxId + 1
 }
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
