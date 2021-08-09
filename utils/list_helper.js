29ce61cd16fa4d557b5c0c3a8ad55ac88553802e
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-08-09 11:43:52 +0000

    Ejercicio 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,20 @@
+// este fichero lo uso para crear las funciones que usaré en los tests, que 
+// realmente no existe puesto serán las propias funciones de mi código
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  } 
+
+  return blogs.reduce((sum, b) => {
+    return sum + b.likes 
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
