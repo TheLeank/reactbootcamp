fa77ae4f0f0fd0bfd6842ffca1f2b5548eaaf055
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-08-19 12:23:45 +0000

    Ejercicio 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -35,9 +35,28 @@ const mostBlogs = (blogs) => {
     return { 'author': _.sample(author).author, 'blogs': _.size(author) }
 }
 
+const mostLikes = (blogs) => {
+  // una util de lodash es crear un objeto lodash, de manera que podemos
+  // encadenar métodos al objeto sin necesidad de llamar a lodash constantemente
+  // y escribir el nombre de la variable sobre la que trabajamos, obteniendo
+  // el resultado final llamando al metodo value
+  const output =
+    _(blogs)
+      .groupBy('author')
+      .map((objs, key) => ({
+          author: key,
+          likes: _.sumBy(objs, 'likes')
+      }))
+      .value()
+  
+  // maxBy no es encadenable usando el objeto lodash, así que lo usamos fuera
+  return _.maxBy(output, 'likes')
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
