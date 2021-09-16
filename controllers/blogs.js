7968c910167d80a5504c5d9f8694b7a8a2cb2d54
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-09-16 11:04:49 +0000

    Ejercicio 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -33,8 +33,20 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toJSON() === decodedToken.id) {
+    await Blog.remove(blog)
+    response.status(204).end()
+  } else {
+    response.status(401).end()
+  }
+  
 })
 
 blogRouter.put('/:id', async (req, res) => {
